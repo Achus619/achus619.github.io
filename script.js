ade59dc8c0d1744a31a28149215e97f71079da6c
achus <achus619@gmail.com>
2021-02-07 15:02:15 -0300

    mejora animación de caída

---
+++ script.js
@@ -0,0 +1,14 @@
+function caer() {
+  const holis = document.querySelector("#holis");
+  const body = document.querySelector("body");
+  const punto = document.querySelector("#punto");
+  const bienvenidx = document.querySelector("#bienvenidx");
+  if (!holis || !body || !punto || !bienvenidx) {
+    console.error("No encontré los elementos");
+    return;
+  }
+  holis.classList.add("holis-subir");
+  body.classList.add("background-scroll");
+  punto.classList.add("punto-cayendo", "ocultar-pulso");
+  bienvenidx.classList.add("bienvenidx-subir");
+}
